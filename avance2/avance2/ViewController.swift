9ba8b00208b74d607ee272f6bfc2853ea4fa4517
Fernando Negrete Pimentel - vn5451i <fnegrete9510@gmail.com>
2023-05-29 11:49:03 -0600

    agregue elementos de vista, revisando porque falla el boton

---
+++ avance2/avance2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  avance2
+//
+//  Created by Fernando Negrete Pimentel on 28/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var changeImageBtn: UIButton!
+    @IBOutlet weak var imageViewPokemon: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+        @IBAction func buttonClicked(_ sender: Any) {
+           // imageViewPokemon.image = UIImage(named: "squertle")
+            print("aqui")
+        }
+    
+
+}
+
